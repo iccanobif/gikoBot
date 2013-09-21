8655e7b14df87d258664583503aa13eee188f2ca
iccanobif <d_evo_lution@hotmail.com>
2013-09-21 14:49:10 +0200

    Replies properly to PING messages.
    Refactoring (dunno if it's better like this or not).

--- gikobot.py
+++ gikobot.py
@@ -8,68 +8,68 @@ network = 'irc.rizon.net'
 port = 6667
 channel = "#gikopoiTest"
 userNameBot = "GikoBot"
+newLine = "\r\n"
 
 #####################
 # CLASS DEFINITIONS #
 #####################
 
-# class Message:
-	# """This represents a message sent from the server"""
-	# def __init__(self, line):
-		# self.line = line
-		# self.splitted = line.split(" ")
-	
+class Message:
+	"""This represents a message sent from the server"""
+	def __init__(self, line):
+		self.line = line
+		self.splitted = line.split(" ")
+
 #I expect lines to look kinda like this: ":iccanobif!iccanobif@fib.fib PRIVMSG #gikopoi :test"	
 
-	# def isPing(self):
-		# return splitted[0] == "PING"
+	def isPing(self):
+		return self.splitted[0] == "PING"
 		
-	# def pingContent(self):
-		# return splitted[1]z
+	def pingContent(self):
+		return self.splitted[1]
 	
-	# def isMessage(self):
-		# return splitted[1] == "PRIVMSG" and splitted[2] == channel
+	def isMessage(self):
+		return self.splitted[1] == "PRIVMSG" and self.splitted[2] == channel
 	
-	# def senderUsername:
-		# return splitted[0].split("!")[0][1:].upper()
+	def messageSenderUsername(self):
+		return self.splitted[0].split("!")[0][1:].upper()
 		
-	# def content(self):
-		# return splitted[3][:6]
-		
-#:iccanobif!iccanobif@fib.fib PRIVMSG #gikopoi :test
-		
-		
-		
-		
+	def messageContent(self):
+		return self.splitted[3][1:]
 
+def sendMessageToChannel(text):
+	irc.send("PRIVMSG " + channel + " :" + text + newLine)
 
 #################
 # PROGRAM START #
 #################
 
-irc = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
-irc.connect ( ( network, port ) )
-irc.send ( "NICK " + userNameBot + "\r\n" )
-irc.send ( "USER " + userNameBot + " 8 * : " + userNameBot + "\r\n" )
-irc.send ( "JOIN " + channel + "\r\n" )
-#irc.send ( 'PRIVMSG #gikopoi :Hello, this is GikoBot. I will try my best to participate constructively in your conversations.\r\n' )
+irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+irc.connect((network, port))
+irc.send("NICK " + userNameBot + newLine)
+irc.send("USER " + userNameBot + " 8 * : " + userNameBot + newLine)
+irc.send("JOIN " + channel + newLine)
+sendMessageToChannel("Hello, this is GikoBot. I will try my best "
+					"to participate constructively in your conversations." + newLine)
 
 currentLine = ""
 
 while True:
 	currentLine = currentLine + irc.recv(1)
 	if currentLine[-1] == '\n':
-		print(currentLine[:-2])
-		# I expect lines to look kinda like this: ":iccanobif!iccanobif@fib.fib PRIVMSG #gikopoi :test"
-		splitted = currentLine.split(" ")
-		if splitted[1] == "PRIVMSG" and splitted[2] == channel:
-			if splitted[3][:6] != ":!quit":
-				username = splitted[0].split("!")[0][1:].upper()
-				irc.send("PRIVMSG " + channel + " :SHUT UP, " + username + "\r\n")
-			else:
-				irc.send ( "PART " + channel + "\r\n" )
-				irc.send ( "QUIT\r\n" )
+		currentLine = currentLine[:-2] # remove '\r\n'
+		print(currentLine)
+		msg = Message(currentLine)
+		if msg.isPing():
+			irc.send("PONG " + msg.pingContent() + newLine)
+		elif msg.isMessage():
+			if msg.messageContent() == "!quit":
+				sendMessageToChannel("SHUTTING DOWN")
+				irc.send("PART " + channel + newLine)
+				irc.send("QUIT" + newLine)
 				irc.close()
 				sys.exit(0)
+			else:
+				sendMessageToChannel("SHUT UP, " + msg.messageSenderUsername())
 				
 		currentLine = ""
