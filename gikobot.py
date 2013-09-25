03aa526dfde2b3c2ab4f4de0b9cec2c9ea5e925f
iccanobif <d_evo_lution@hotmail.com>
2013-09-25 15:34:16 +0200

    Added rudimental error handling

--- gikobot.py
+++ gikobot.py
@@ -17,27 +17,27 @@ newLine = "\r\n"
 #####################
 
 class Message:
-	"""This represents a message sent from the server"""
-	def __init__(self, line):
-		self.line = line
-		self.splitted = line.split(" ")
+    """This represents a message sent from the server"""
+    def __init__(self, line):
+        self.line = line
+        self.splitted = line.split(" ")
 
 #I expect lines to look kinda like this: ":iccanobif!iccanobif@fib.fib PRIVMSG #gikopoi :test"	
 
-	def isPing(self):
-		return self.splitted[0] == "PING"
-		
-	def pingContent(self):
-		return self.splitted[1]
-	
-	def isMessage(self):
-		return self.splitted[1] == "PRIVMSG" and self.splitted[2] == channel
-	
-	def messageSenderUsername(self):
-		return self.splitted[0].split("!")[0][1:].upper()
-		
-	def messageContent(self):
-		output = ""
+    def isPing(self):
+        return self.splitted[0] == "PING"
+        
+    def pingContent(self):
+        return self.splitted[1]
+    
+    def isMessage(self):
+        return self.splitted[1] == "PRIVMSG" and self.splitted[2] == channel
+    
+    def messageSenderUsername(self):
+        return self.splitted[0].split("!")[0][1:].upper()
+        
+    def messageContent(self):
+        output = ""
         for s in xrange(2, len(self.splitted) - 1):
             output = output + s + " "
         return output[:-1]
@@ -47,10 +47,10 @@ class Message:
 #############
 
 def sendMessageToChannel(text):
-	irc.send("PRIVMSG " + channel + " :" + text + newLine)
+    irc.send("PRIVMSG " + channel + " :" + text + newLine)
 
 def getRandomKaomoji():
-	return kaomojiList[randint(0, len(kaomojiList)-1)]
+    return kaomojiList[randint(0, len(kaomojiList)-1)]
 
 ##########
 # QUOTES #
@@ -68,7 +68,7 @@ def sendQuote():
     quote = db.execute("SELECT quote FROM quotes ORDER BY random() LIMIT 1").fetchone()[0]
     sendMessageToChannel(quote)
     db.close()
-	
+
 #################
 # PROGRAM START #
 #################
@@ -88,35 +88,47 @@ irc.send("NICK " + userNameBot + newLine)
 irc.send("USER " + userNameBot + " 8 * : " + userNameBot + newLine)
 irc.send("JOIN " + channel + newLine)
 #sendMessageToChannel("Hello, this is GikoBot. I will try my best "
-#					"to participate constructively in your conversations." + newLine)
+#                     "to participate constructively in your conversations." + newLine)
 
 currentLine = ""
 
 while True:
-	currentLine = currentLine + irc.recv(1)
-	if currentLine[-1] == '\n':
-		currentLine = currentLine[:-2] # remove '\r\n'
-		print(currentLine)
-		msg = Message(currentLine)
-		if msg.isPing():
-			irc.send("PONG " + msg.pingContent() + newLine)
-			irc.send("JOIN " + channel + newLine) #in case i was ping'd before receiving all the MOTD (yotsubano.me does that, for example)
-		elif msg.isMessage():
-            command = msg.messageContent().split(" ")[0]
-        
-			if command == "!quit":
-				sendMessageToChannel("SHUTTING DOWN")
-				irc.send("PART " + channel + newLine)
-				irc.send("QUIT" + newLine)
-				irc.close()
-				sys.exit(0)
-			elif command == "!kaomoji":
-				sendMessageToChannel(getRandomKaomoji())
-            elif command == "!addquote"
-                addQuote(msg)
-            elif command == "!quote"
-                sendQuote(msg)
-			#else:
-			#	sendMessageToChannel("SHUT UP, " + msg.messageSenderUsername())
-				
-		currentLine = ""
+    currentLine = currentLine + irc.recv(1)
+    if currentLine[-1] == '\n':
+    
+        try:
+            currentLine = currentLine[:-2] # remove '\r\n'
+            print(currentLine)
+            msg = Message(currentLine)
+            if msg.isPing():
+                irc.send("PONG " + msg.pingContent() + newLine)
+                irc.send("JOIN " + channel + newLine) #in case i was ping'd before receiving all the MOTD (yotsubano.me does that, for example)
+            elif msg.isMessage():
+                command = msg.messageContent().split(" ")[0]
+            
+                if command == "!quit":
+                    sendMessageToChannel("SHUTTING DOWN")
+                    irc.send("PART " + channel + newLine)
+                    irc.send("QUIT" + newLine)
+                    irc.close()
+                    sys.exit(0)
+                elif command == "!kaomoji":
+                    sendMessageToChannel(getRandomKaomoji())
+                elif command == "!addquote"
+                    addQuote(msg)
+                elif command == "!quote"
+                    sendQuote(msg)
+                #else:
+                #   sendMessageToChannel("SHUT UP, " + msg.messageSenderUsername())
+        except:
+            errorMessage = "Unexpected error:", sys.exc_info()[0]
+            print(errorMessage)
+            try:
+                sendMessageToChannel(errorMessage)
+            except:
+                pass
+                
+                
+            
+                
+    currentLine = ""
