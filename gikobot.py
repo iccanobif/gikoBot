af2dd90086c883ed18369a563ccc7f4579cb0e77
iccanobif <d_evo_lution@hotmail.com>
2015-03-13 19:13:24 +0100

    Updated with what's in "production" right now

--- gikobot.py
+++ gikobot.py
@@ -5,6 +5,9 @@ import sqlite3
 import time
 import traceback
 import datetime
+import os
+import re
+import codecs
 
 ############
 # SETTINGS #
@@ -39,7 +42,7 @@ class Message:
         return self.splitted[1] == "PRIVMSG" and self.splitted[2] == channel
     
     def messageSenderUsername(self):
-        return self.splitted[0].split("!")[0][1:].upper()
+        return self.splitted[0].split("!")[0][1:]
         
     def messageContent(self):
         output = ""
@@ -55,8 +58,10 @@ def printDebug(text):
     print("[" + datetime.datetime.strftime(datetime.datetime.now(), "%d/%m/%Y %H:%M") + "] " + text)
 
 def sendMessageToChannel(text):
-    print(type(text))
     irc.send("PRIVMSG " + channel + " :" + text + newLine)
+
+def sendMessageToNick(nick, text):
+    irc.send("PRIVMSG " + nick + " :" + text + newLine)
 
 def getRandomKaomoji():
     return kaomojiList[randint(0, len(kaomojiList)-1)]
@@ -91,6 +96,8 @@ f = open(kaomojiPath, "r")
 kaomojiList = f.readlines()
 f.close()
 
+penisLength = 0
+
 #Init database:
 db = sqlite3.connect("gikobot.sqlite")
 db.execute("CREATE TABLE IF NOT EXISTS quotes (quote TEXT)")
@@ -112,6 +119,8 @@ currentLine = ""
 
 while True:
     currentLine = currentLine + irc.recv(1)
+    #TODO: Handle len(currentLine) == 0 (that means we got disconnected and 
+    #      have to reconnect
     if currentLine[-1] == '\n':
     
         try:
@@ -123,7 +132,7 @@ while True:
                 irc.send("PONG " + msg.pingContent() + newLine)
                 irc.send("JOIN " + channel + newLine) #in case i was ping'd before receiving all the MOTD (yotsubano.me does that, for example)
             elif msg.isMessage():
-                command = msg.messageContent().split(" ")[0]
+                command = msg.messageContent().split(" ")[0].lower()
 #                if command == "!quit":
 #                    sendMessageToChannel("SHUTTING DOWN")
 #                    irc.send("PART " + channel + newLine)
@@ -136,13 +145,50 @@ while True:
                     addQuote(msg)
                 elif command == "!quote":
                     sendQuote()
+		elif command == "!quotecount":
+                    db = sqlite3.connect("gikobot.sqlite")
+                    row = db.execute("SELECT count(1) FROM quotes").fetchone()
+                    count = row[0]
+		    db.close()
+		    sendMessageToChannel("Total quotes: " + str(count))
 		elif command == "!help":
-		    sendMessageToChannel("Commands available: !kaomoji !quote !addquote")
-                #else:
-                #   sendMessageToChannel("SHUT UP, " + msg.messageSenderUsername())
+		    sendMessageToChannel("Commands available: !kaomoji !quote !addquote !quotecount !touchPenis !figlet !dumpquotes")
+		elif command == "!kaomojicount":
+		    sendMessageToChannel("Kaomoji available: " + str(len(kaomojiList)))
+		elif command == "!touchpenis":
+		    sendMessageToChannel(msg.messageSenderUsername() + " gently touches the penis!")
+		    penisLength += 1
+		    penis = "8"
+		    for i in xrange(0, penisLength):
+		        penis = penis + "="
+	            penis = penis + "D"
+		    sendMessageToChannel(penis)
+		elif command == "!figlet":
+                    input = msg.messageContent()[8:]
+		    input = re.sub("[^a-zA-Z0-9 ]", "", input)[0:20]
+		    print(input)
+		    output = os.popen("./figlet -d figletfonts \"" + input + "\"").readlines()
+		    for line in output:
+		        sendMessageToChannel(line)
+                        time.sleep(0.5)
+                elif command == "!dumpquotes":
+                    # would be dangerous if the list grows huge.. well it isn't huge yet, so nothing to worry about.
+                    # from time import sleep
+                    db = sqlite3.connect("gikobot.sqlite")
+                    quoteList = db.execute("SELECT quote FROM quotes").fetchall()
+                    db.close()
+                    for quote in quoteList:
+                        sendMessageToNick(msg.messageSenderUsername().encode('utf8'), quote[0].encode('utf8'))
+                        # do a sleep here to avoid floods
+                        time.sleep(0.5)
+                    print("finished dumping to", msg.messageSenderUsername())
+                elif command == "!pop":
+		    sendMessageToChannel("not implemented lol")
+		
         except:
 	    traceback.print_exc()
             try:
+		#TODO: if the error was a disconnection from the server?
                 sendMessageToChannel(errorMessage)
             except:
 	    	print("some error?")
